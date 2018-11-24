92fec1b6742ff4d2c2ae863764e218544aff2035
Elliot Alexander <epurvis97@gmail.com>
2018-11-24 15:35:11 +0000

    [Refactor] Shrinking routing, adding Costin's requirements.
    One day he'll push

--- backend/schema.py
+++ backend/schema.py
@@ -1,10 +1,7 @@
 import graphene
-from graphene import relay
 
 class Query(graphene.ObjectType):
     helloworld = graphene.String(argument=graphene.String(default_value="Kill ourselves"))
-
-    def resolve_helloworld(self, info, argument):
-        return "Hello world! Let's " + argument
+    resolve_helloworld = lambda self, info, argument: "Hello world! Let's " + argument
 
 schema = graphene.Schema(query=Query)
