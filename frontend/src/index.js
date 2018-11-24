40837856dd1cc8aab01dd4eeb0ba4c06cf3cacf4
hrybrn <harryjgbrown@gmail.com>
2018-11-24 14:40:14 +0000

    apollo client and test setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,19 @@ import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+
+const client = new ApolloClient({
+	uri: 'http://localhost:4000'
+});
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>
+	, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
