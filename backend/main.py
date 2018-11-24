266b68de96c55b2343eff3d77f56f61c96f5951c
Elliot Alexander <epurvis97@gmail.com>
2018-11-24 15:34:45 +0000

    [Refactor] Shrinking routing

--- backend/main.py
+++ backend/main.py
@@ -2,14 +2,8 @@ from flask import Flask
 from flask_graphql import GraphQLView
 from schema import schema
 
-
 app = Flask(__name__)
-
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
-
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
 # [END gae_python37_app]
