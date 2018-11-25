89a2b12a6cc4ee5623d0fe4b49a53ca13bb9e417
bradsharp <bradleysharpie@gmail.com>
2018-11-25 17:22:07 +0000

    Added test endpoint

--- backend/resolvers.py
+++ backend/resolvers.py
@@ -1,1 +1,10 @@
 helloworld = lambda value, info, **args: 'Hello ' + args['name'] + '! Let\'s kill ourselves' if 'name' in args else 'Hello world! Let\'s kill ourselves'
+
+def entityTest(value, info, **args):
+	task = datastore.Entity(client.key('Test', args['name']))
+	task.update({
+		'category': 'Personal',
+		'done': False,
+		'priority': 4,
+		'description': 'Learn Cloud Datastore'
+	})
