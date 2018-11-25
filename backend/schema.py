b4395893ca6d069eaf8378ba4271ef452ce10e59
hrybrn <harryjgbrown@gmail.com>
2018-11-25 16:01:39 +0000

    clean up and proof of concept on nested endpoints

--- backend/schema.py
+++ backend/schema.py
@@ -1,17 +1,16 @@
 from graphql_tools import build_executable_schema
+from resolvers import helloworld
 
 with open('schema.graphql', 'r') as schema_file:
     source_schema = schema_file.read()
 
-def helloworld(self, info, **args):
-    if 'name' in args:
-        return 'Hello world! Let\'s ' + args['name']
-    else:
-        return 'Hello world! Let\'s kill ourselves'
 
 resolvers = {
     'RootQuery': {
-        'helloworld': helloworld
+        'helloworld': lambda value, info, **args: 'HelloWorld'
+    },
+    'HelloWorld': {
+        'hi': helloworld
     }
 }
 schema = build_executable_schema(source_schema, resolvers)
