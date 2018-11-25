d1482fe6dac21430d95e467ee6ce8e27936ac501
bradsharp <bradleysharpie@gmail.com>
2018-11-25 18:56:34 +0000

    Added in command and shell script for database

--- backend/run.cmd
+++ backend/run.cmd
@@ -1,5 +1,6 @@
 start gcloud beta emulators datastore start --no-store-on-disk
 timeout /t 3 /nobreak
-call gcloud beta emulators datastore env-init
-call set_vars.cmd
+call gcloud beta emulators datastore env-init > set_env.cmd
+call set_env
+del set_env.cmd
 python main.py
