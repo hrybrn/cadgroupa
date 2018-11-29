54bb66f435fbf10b05fbc62a00b54ac96af35745
hrybrn <harryjgbrown@gmail.com>
2018-11-29 17:12:55 +0000

    added panels into App

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,41 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
+import Navigation from '../Navigation/Navigation';
+import LeftPanel from '../LeftPanel/LeftPanel';
+import RightPanel from '../RightPanel/RightPanel';
 
 class App extends Component {
-	render() {
-		const message = this.props.data.loading ? 'Loading...' : this.props.data.hello;
-		
+	render() {		
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>{message}</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</header>
+				<Navigation />
+				<LeftPanel />
+				<RightPanel />
 			</div>
 		);
 	}
 }
 
-export const helloworld = gql`query($name: String) {
-	hello(name: $name)
-}`;
-
-export default graphql(helloworld, {
-	options: {
-		variables: {
-			name: 'Chris'
-		}
-	}
-})(App);
+export default App;
