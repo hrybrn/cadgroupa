4a4bc6b2ab377b8fa7ce26cb54b0df121b401b59
hrybrn <harryjgbrown@gmail.com>
2018-12-01 14:42:44 +0000

    light theme added

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import Navigation from '../Navigation/Navigation';
-import LeftPanel from '../LeftPanel/LeftPanel';
 import RightPanel from '../RightPanel/RightPanel';
 
 class App extends Component {
+    theme = createMuiTheme({
+        palette: {
+            type: 'light',
+        },
+        typography: {
+            useNextVariants: true,
+        },
+    });
+
     render() {		
         return (
-            <div className="App">
+            <MuiThemeProvider theme={this.theme}>
                 <Navigation />
-                <LeftPanel />
                 <RightPanel />
-            </div>
+            </MuiThemeProvider>
         );
     }
 }
