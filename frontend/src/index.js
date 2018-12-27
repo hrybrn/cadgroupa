d2ba4a1a11b550685e38d1396d04810bca8d0298
Elliot Alexander <epurvis97@gmail.com>
2018-12-27 03:12:08 +0000

    [Fix] Removing old AuthContext.
    All of this has been rewritten. See future commits.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from 'serviceWorker';
 
-import { AuthProvider } from 'components/Context/AuthContext';
 
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
@@ -17,9 +16,7 @@ const client = new ApolloClient({
 ReactDOM.render(
     <BrowserRouter>
         <ApolloProvider client={client}>
-            <AuthProvider>
-                <App/>
-            </AuthProvider>
+            <App/>
         </ApolloProvider>
     </BrowserRouter>,
     document.getElementById('root'));
