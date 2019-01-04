1fbc75b1832ef604185408cd82da798afb983526
Christopher Coles <cbc1g16@soton.ac.uk>
2019-01-04 16:54:44 +0000

    [Feat] Made game tile functional
    made so only one card tile could be active, got rid of number of players

--- frontend/src/components/LeftPanel/LeftPanel.js
+++ frontend/src/components/LeftPanel/LeftPanel.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { graphql } from 'react-apollo';
-import {GridList, GridListTile, FormLabel,withStyles, ListSubheader, CircularProgress, Checkbox, RadioGroup, Radio, FormControlLabel} from '@material-ui/core';
+import {GridList, GridListTile,withStyles, ListSubheader, CircularProgress, Checkbox} from '@material-ui/core';
 import CardTile from 'components/CardTile/CardTile';
 import { gameQuery } from 'queries/games'; 
 
@@ -12,6 +12,9 @@ const styles = () => ({
         height: '50px !important',
         'text-align': 'center',
         margin: '0 auto',
+    },
+    radio: {
+        left: 0
     }
 });
 
@@ -38,19 +41,20 @@ class LeftPanel extends Component {
                 </GridListTile>
             );
         } else {
-            return data.game.map(game =>
+            return this.props.data.games.map(game =>
                 <CardTile
                     key={game.id}
-                    activated={this.activated(game.modes)}
+                    activated={this.activated(game)}
                     deactivated={this.deactivated}
+                    active={this.state.selectedGame == game.name ? true : false}
                     {...game}
                 />
             );
         }
     }
 
-    activated(newModes) {
-        this.setState({modes: newModes});
+    activated = (game) => {
+        this.setState({modes: game.modes, selectedGame: game.name});
     }
 
     deactivated() {
@@ -68,27 +72,7 @@ class LeftPanel extends Component {
                 </GridListTile>
         );
     }
-    renderNumberOfPlayers = () => {
-        return( 
-            <RadioGroup
-                value={this.state.noOfPlayers}
-                onChange={this.handleChange}
-            >
-                { [2,3,4,5,6].map(
-                    (number) =>
-                        <FormControlLabel
-                            
-                            value={number}
-                            control={<Radio color="primary" />}
-                            label={number}
-                            labelPlacement="start"
-                            key={number}
-                        />
-                )
-                }
-            </RadioGroup>
-        );
-    }
+
     render() {
         const { gridList } = this.props.classes;
         return (
@@ -105,10 +89,7 @@ class LeftPanel extends Component {
                     </GridListTile>
                     {this.renderModes}
                 </GridList>
-                <FormLabel>Number of Players</FormLabel>
-                {
-                    this.renderNumberOfPlayers()
-                }
+                
             </Fragment>
         );
     }
