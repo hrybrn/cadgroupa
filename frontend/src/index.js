d1840c3137d46c30d7214ef894437d87ce295ea7
hrybrn <harryjgbrown@gmail.com>
2019-01-05 16:56:04 +0000

    [Fix] Environment variables now working correctly

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from 'serviceWorker';
 
-
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 import App from 'components/App/App';
 
-const client = new ApolloClient({
-    uri: 'http://127.0.0.1:8080/graphql',
-});
+// eslint-disable-next-line no-undef
+const uri = process.env.REACT_APP_GRAPHQL_ENDPOINT;
+const client = new ApolloClient({ uri });
 
 ReactDOM.render(
     <BrowserRouter>
