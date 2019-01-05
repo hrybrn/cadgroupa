91bee7c4337bb22c8cd3711e2bcf4759d3922d49
hrybrn <harryjgbrown@gmail.com>
2019-01-05 18:13:14 +0000

    [Fix] Broken schema and resolvers

--- frontend/src/queries/discord.js
+++ frontend/src/queries/discord.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 // Putting repeatable queries here to save line count.
 export const get_user_obj = gql`
-    query Getuser($token: String!) {
+    query getuser($token: String!) {
         discord {
             getuser(token: $token)
         }
