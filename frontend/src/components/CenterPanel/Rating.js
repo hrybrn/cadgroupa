df82fe48d15f22b99e477a5d0d3572d58c171f13
hrybrn <harryjgbrown@gmail.com>
2019-01-05 19:19:05 +0000

    [Feat] connected search button to center panel

--- frontend/src/components/CenterPanel/Rating.js
+++ frontend/src/components/CenterPanel/Rating.js
@@ -5,29 +5,23 @@ import { graphql } from 'react-apollo';
 import ThumbsUp from '@material-ui/icons/ThumbUp';
 import ThumbsDown from '@material-ui/icons/ThumbDown';
 
-class Searching extends Component {
-    constructor(props) {
-        super(props);
-    }
-
+class Rating extends Component {
     render() {
-        return (
-            <Grid
-                container
-                spacing={0}
-                alignItems="center"
-                justify="center"
-                style={{ minHeight: '100vh' }}
-            >
-                {this.data.poll.players.map(player =>
-                    <Fragment key={player}>
-                        <FormLabel>player.name</FormLabel>
-                        <ThumbsUp type="button" onClick={this.sendFeedback.bind(this, player.id, true)}></ThumbsUp>
-                        <ThumbsDown type="button" onClick={this.sendFeedback.bind(this, player.id, false)}></ThumbsDown>
-                    </Fragment>
-                )   }
-            </Grid>
-        );
+        return <Grid
+            container
+            spacing={0}
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: '100vh' }}
+        >
+            {this.data.poll.players.map(player =>
+                <Fragment key={player}>
+                    <FormLabel>player.name</FormLabel>
+                    <ThumbsUp type="button" onClick={this.sendFeedback.bind(this, player.id, true)}></ThumbsUp>
+                    <ThumbsDown type="button" onClick={this.sendFeedback.bind(this, player.id, false)}></ThumbsDown>
+                </Fragment>
+            )   }
+        </Grid>;
     }
 
     sendFeedback(playerID, good) {
@@ -35,20 +29,19 @@ class Searching extends Component {
     }
 }
 
-export const rateMutation = gql`mutation RatePlayer($playerID: String!, $good: Boolean){
-        rating(playerID: $playerID, good: $good) {
-            success
-        }
+// eslint-disable-next-line no-unused-vars
+const rateMutation = gql`mutation RatePlayer($playerID: String!, $good: Boolean) {
+    rating(playerID: $playerID, good: $good) {
+        success
     }
 }`;
 
-export const playersQuery = gql`{
+const playersQuery = gql`{
     data {
         poll {
-            players{
-            }
+            players
         }
     }
 }`;
 
-export default graphql(playersQuery, rateMutation)(Searching);
+export default graphql(playersQuery)(Rating);
