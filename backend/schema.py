0c759be51946c0cf7d00aba3f1daeed8c359d43a
hrybrn <harryjgbrown@gmail.com>
2019-01-05 20:31:21 +0000

    [Feat] setup mutations

--- backend/schema.py
+++ backend/schema.py
@@ -23,152 +23,171 @@ from resolvers import user, userfriends, games, helloWorld, entityTest, register
 
 # Note that a different schema would be defined for mutations - this is not a complete schema object (see below)
 queryschema = GraphQLObjectType(
-		"RootQueryType",
-		lambda: {
-			"discord": GraphQLField(
-				type=GraphQLObjectType(
-					name="user",
-					fields={
-						"getuser" : GraphQLField(
-							type= GraphQLString,
-							args={
-								'token' : GraphQLArgument(
-									type=GraphQLString
-								)
-							},
-							resolver=user
-						),
-                        "userfriends" : GraphQLField(
-							type= GraphQLString,
-							args={
-								'token' : GraphQLArgument(
-									type=GraphQLString
-								)
-							},
-							resolver=userfriends
-						)
-					}
-				),
-				# This seems to need to be defined, although it's not used?
-				resolver=helloWorld	
-			),
-            "helloworld": GraphQLField(
-                type=GraphQLString,
-                args={
-                    "name": GraphQLArgument(GraphQLString)
-                },
-                resolver=helloWorld
-            ),
-			"recentPlayers": GraphQLField(
-				type=GraphQLList(GraphQLString),
-				args={
-					"token": GraphQLArgument(GraphQLString)
-				},
-				resolver=getRecentPlayers
-			),
-			"requestsInSystem": GraphQLField(
-                type=GraphQLString,
-                args={
-                    "gameId": GraphQLArgument(GraphQLString)
-                },
-                resolver=requestsInSystem
-            ),
-			"matchmaking": GraphQLField(
-				type=GraphQLObjectType(
-					name="matchmakingOptions",
-					fields={
-                        "rating":GraphQLField(
-                            type=GraphQLString,
-                            args={
-                                "playerID":GraphQLArgument(GraphQLString),
-                                "good":GraphQLArgument(GraphQLBoolean),
-                                "token":GraphQLArgument(GraphQLString)
-                            },
-                            resolver=changeUserScore
-                        ),
-						"registerSearch": GraphQLField(
-							type=GraphQLObjectType(
-								name="registerResponse",
-								fields={
-									"gameID": GraphQLField(GraphQLString),
-									"mode": GraphQLField(GraphQLString),
-									"players": GraphQLField(GraphQLInt),
-									"rank": GraphQLField(GraphQLInt),
-									"lat": GraphQLField(GraphQLFloat),
-									"lon": GraphQLField(GraphQLFloat)
-								}
-							),
-							args={
-								"token": GraphQLArgument(GraphQLString),
-								"gameID": GraphQLArgument(GraphQLString),
-								"mode": GraphQLArgument(GraphQLString),
-								"players": GraphQLArgument(GraphQLInt),
-								"rank": GraphQLArgument(GraphQLInt),
-								"lat": GraphQLArgument(GraphQLFloat),
-								"lon": GraphQLArgument(GraphQLFloat)
-							},
-							resolver=registerSearch
-						),
-                        "poll": GraphQLField(
-							type=GraphQLObjectType(
-								name="pollResponse",
-								fields={
-									"token": GraphQLField(
-										type=GraphQLString
-									),
-									"success": GraphQLField(
-										type=GraphQLBoolean
-									),
-									"playerDiscordIDs": GraphQLField(GraphQLList(GraphQLString)),
-									"groupDMURL": GraphQLField(GraphQLString)
-								},
-							),
-							args={
-								"token": GraphQLArgument(GraphQLString)
-							},
-							resolver=pollSearch
-							
-						),
-					}
-				),
-				# This seems to need to be defined, although it's not used?
-				resolver=helloWorld	
-			),
-            "games": GraphQLField(
-					type=GraphQLList(
-						type=GraphQLObjectType(
-							name="game",
-							fields={
-								"id": GraphQLField(GraphQLString),
-								"name": GraphQLField(GraphQLString),
-								"icon": GraphQLField(GraphQLString),
-								"maxplayers": GraphQLField(GraphQLInt),
-								"minplayers": GraphQLField(GraphQLInt),
-								"minage": GraphQLField(GraphQLInt),
-								"description": GraphQLField(GraphQLString),
-								"website": GraphQLField(GraphQLString),
-								"modes": GraphQLField(GraphQLList(
-									type=GraphQLObjectType(
-										name="mode",
-										fields={
-											"name": GraphQLField(GraphQLString),
-											"players": GraphQLField(GraphQLInt)
-										}
-									)
-								))
-							}
-						)
+	name="RootQueryType",
+	fields={
+		"discord": GraphQLField(
+			type=GraphQLObjectType(
+				name="user",
+				fields={
+					"getuser" : GraphQLField(
+						type= GraphQLString,
+						args={
+							'token' : GraphQLArgument(
+								type=GraphQLString
+							)
+						},
+						resolver=user
 					),
-					resolver=games
+					"userfriends" : GraphQLField(
+						type= GraphQLString,
+						args={
+							'token' : GraphQLArgument(
+								type=GraphQLString
+							)
+						},
+						resolver=userfriends
+					)
+				}
 			),
-            "helloworld": GraphQLField(
-                type=GraphQLString,
-                args={
-                    'name': GraphQLArgument(GraphQLString)
-                },
-                resolver=helloWorld
-            )
-		}
+			# This seems to need to be defined, although it's not used?
+			resolver=helloWorld	
+		),
+		"helloworld": GraphQLField(
+			type=GraphQLString,
+			args={
+				"name": GraphQLArgument(GraphQLString)
+			},
+			resolver=helloWorld
+		),
+		"recentPlayers": GraphQLField(
+			type=GraphQLList(GraphQLString),
+			args={
+				"token": GraphQLArgument(GraphQLString)
+			},
+			resolver=getRecentPlayers
+		),
+		"requestsInSystem": GraphQLField(
+			type=GraphQLString,
+			args={
+				"gameId": GraphQLArgument(GraphQLString)
+			},
+			resolver=requestsInSystem
+		),
+		"matchmaking": GraphQLField(
+			type=GraphQLObjectType(
+				name="matchmakingQuery",
+				fields={
+					"register": GraphQLField(
+						type=GraphQLObjectType(
+							name="registerResponse",
+							fields={
+								"gameID": GraphQLField(GraphQLString),
+								"mode": GraphQLField(GraphQLString),
+								"players": GraphQLField(GraphQLInt),
+								"rank": GraphQLField(GraphQLInt),
+								"lat": GraphQLField(GraphQLFloat),
+								"lon": GraphQLField(GraphQLFloat)
+							}
+						),
+						args={
+							"token": GraphQLArgument(GraphQLString),
+							"gameID": GraphQLArgument(GraphQLString),
+							"mode": GraphQLArgument(GraphQLString),
+							"players": GraphQLArgument(GraphQLInt),
+							"rank": GraphQLArgument(GraphQLInt),
+							"lat": GraphQLArgument(GraphQLFloat),
+							"lon": GraphQLArgument(GraphQLFloat)
+						},
+						resolver=registerSearch
+					)
+				}
+			),
+			# This seems to need to be defined, although it's not used?
+			resolver=helloWorld	
+		),
+		"games": GraphQLField(
+				type=GraphQLList(
+					type=GraphQLObjectType(
+						name="game",
+						fields={
+							"id": GraphQLField(GraphQLString),
+							"name": GraphQLField(GraphQLString),
+							"icon": GraphQLField(GraphQLString),
+							"maxplayers": GraphQLField(GraphQLInt),
+							"minplayers": GraphQLField(GraphQLInt),
+							"minage": GraphQLField(GraphQLInt),
+							"description": GraphQLField(GraphQLString),
+							"website": GraphQLField(GraphQLString),
+							"modes": GraphQLField(GraphQLList(
+								type=GraphQLObjectType(
+									name="mode",
+									fields={
+										"name": GraphQLField(GraphQLString),
+										"players": GraphQLField(GraphQLInt)
+									}
+								)
+							))
+						}
+					)
+				),
+				resolver=games
+		),
+		"helloworld": GraphQLField(
+			type=GraphQLString,
+			args={
+				'name': GraphQLArgument(GraphQLString)
+			},
+			resolver=helloWorld
+		)
+	}
 )
 
-schema = GraphQLSchema(query=queryschema)
+mutationschema = GraphQLObjectType(
+	name="RootMutationType",
+	fields={
+		"matchmaking": GraphQLField(
+			type=GraphQLObjectType(
+				name="matchmakingMutations",
+				fields={
+					"rating":GraphQLField(
+						type=GraphQLObjectType(
+							name="ratingResponse",
+							fields={
+								"success":GraphQLField(GraphQLBoolean)
+							}
+						),
+						args={
+							"playerID":GraphQLArgument(GraphQLString),
+							"good":GraphQLArgument(GraphQLBoolean),
+							"token":GraphQLArgument(GraphQLString)
+						},
+						resolver=changeUserScore
+					),
+					"poll": GraphQLField(
+						type=GraphQLObjectType(
+							name="pollResponse",
+							fields={
+								"token": GraphQLField(
+									type=GraphQLString
+								),
+								"success": GraphQLField(
+									type=GraphQLBoolean
+								),
+								"playerDiscordIDs": GraphQLField(GraphQLList(GraphQLString)),
+								"groupDMURL": GraphQLField(GraphQLString)
+							},
+						),
+						args={
+							"token": GraphQLArgument(GraphQLString)
+						},
+						resolver=pollSearch
+					),
+				}
+			),
+			# This seems to need to be defined, although it's not used?
+			resolver=helloWorld	
+		)
+	}
+)
+
+schema = GraphQLSchema(query=queryschema, mutation=mutationschema)
