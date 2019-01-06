1558d51e035badc0ce0c476ffe4c8835574763e7
Elliot Alexander <ep1e16@soton.ac.uk>
2019-01-06 14:01:34 +0000

    [Feat] Added backend support for checking users discord server status, adding to server.

--- backend/discord.py
+++ backend/discord.py
@@ -6,6 +6,32 @@ from graphql import GraphQLError
 def getuserobj(token):
 	return discord_req(token, 'https://discordapp.com/api/users/@me')
 
+
+def checkuserguild(token, userid):
+	guildid = '531408802357182484'
+	headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {0}'.format(token)}
+	response = requests.get('https://discordapp.com/api/guilds/'+guildid + '/members/'+userid, headers=headers)
+	if(response.status_code == 200):
+		return True
+	else:
+		return False
+
+
+def adduserguild(token, userid):
+	print(token + ' : ' + userid, sys.stderr)
+	guildid = '531408802357182484'
+	headers = {'Content-Type': 'application/json', 'Authorization': 'Bot {0}'.format('NTE5NTI2MDYxODY0Nzc5ODA4.DxOVxg.FgnV-M1j_aI6eLisJ1vaIRz70q4')}
+	print(headers, sys.stderr)
+	jsontext = {'access_token' : token}
+	response = requests.put('https://discordapp.com/api/guilds/531408802357182484/members/'+userid, headers=headers, json=jsontext)
+	if(response.status_code == 200):
+		return True
+	else:
+		print(response.content, sys.stderr)
+		print(response.status_code, sys.stderr)
+		return False
+
+
 def discord_req(token, uri):
 	api_token = token
 	headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {0}'.format(api_token)}
