349be8c25fa9d38a1d182da723ac04b7dfb5e3d3
bradsharp <bradleysharpie@gmail.com>
2019-01-06 09:27:00 +0000

    Improved code

--- backend/users.py
+++ backend/users.py
@@ -5,49 +5,47 @@ MAX_RECENT_PLAYERS = 20
 client = datastore.Client()
 
 def createUser(userId):
-	key = client.key('User', userId)
-	request = datastore.Entity(key)
-	request.update({
+	key = getUserKey(userId)
+	user = datastore.Entity(key)
+	user.update({
 		'recentPlayers': [],
 		'toxicity': 1000,
 		'banned': False
 	})
-	client.put(request)
-	return request
+	client.put(user)
+	return user
+
+def getUser(userId):
+	key = client.key('User', userId)
+	return client.get(key)
 
 def getRecentPlayers(userId):
-	key = client.key('User', userId)
-	request = client.get(key)
-	return request['recentPlayers'] if request['recentPlayers'] else []
+	user = getUser(userId)
+	return user['recentPlayers'] if user['recentPlayers'] else []
 
 def addRecentPlayers(userId, players):
-	key = client.key('User', userId)
-	request = client.get(key)
-	recentPlayers = request['recentPlayers'] if request['recentPlayers'] else []
+	user = getUser(userId)
+	recentPlayers = user['recentPlayers'] if user['recentPlayers'] else []
 	recentPlayers.extend(players)
 	if len(recentPlayers) > MAX_RECENT_PLAYERS:
 		for i in range(0, len(recentPlayers) - MAX_RECENT_PLAYERS):
 			recentPlayers.pop(0)
-	request.update({
+	user.update({
 		'recentPlayers': recentPlayers
 	})
-	client.put(request)
+	client.put(user)
 
-def changePlayerRating(userId, rating_change):
-	key = client.key('User', userId)
-	request = client.get(key)
-
-	score = request['toxicity']
+def changePlayerRating(userId, recipient, downVote):
+	user = getUser(userId)
+	score = user['toxicity']
 	score = score - 50 if rating_change else score + 50
-	request.update({
+	user.update({
 		'toxicity': score,
 	})
-	client.put(request)
+	client.put(user)
 	return True
 
-
 def checkPlayerRating(userId):
-	key = client.key('User', userId)
-	request = client.get(key)
-	score = request['toxicity'] if request['toxicity'] else 0
+	user = getUser(userId)
+	score = user['toxicity'] if user['toxicity'] else 0
 	return True if score > 0 else False
