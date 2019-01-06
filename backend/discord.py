93aa5969bd885221174532370658e0701f6f563e
Elliot Alexander <epurvis97@gmail.com>
2019-01-06 14:05:48 +0000

    [Style] Removing debugging comments.

--- backend/discord.py
+++ backend/discord.py
@@ -11,26 +11,14 @@ def checkuserguild(token, userid):
 	guildid = '531408802357182484'
 	headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {0}'.format(token)}
 	response = requests.get('https://discordapp.com/api/guilds/'+guildid + '/members/'+userid, headers=headers)
-	if(response.status_code == 200):
-		return True
-	else:
-		return False
-
+	return (response.status_code == 200)
 
 def adduserguild(token, userid):
-	print(token + ' : ' + userid, sys.stderr)
 	guildid = '531408802357182484'
 	headers = {'Content-Type': 'application/json', 'Authorization': 'Bot {0}'.format('NTE5NTI2MDYxODY0Nzc5ODA4.DxOVxg.FgnV-M1j_aI6eLisJ1vaIRz70q4')}
-	print(headers, sys.stderr)
 	jsontext = {'access_token' : token}
 	response = requests.put('https://discordapp.com/api/guilds/531408802357182484/members/'+userid, headers=headers, json=jsontext)
-	if(response.status_code == 200):
-		return True
-	else:
-		print(response.content, sys.stderr)
-		print(response.status_code, sys.stderr)
-		return False
-
+	return (response.status_code == 201)
 
 def discord_req(token, uri):
 	api_token = token
