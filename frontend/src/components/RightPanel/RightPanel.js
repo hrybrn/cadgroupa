73fc1507f78897b498aea71cac671b6f7b413879
Christopher Coles <cbc1g16@soton.ac.uk>
2019-01-06 10:20:14 +0000

    [Feat] Added RightPanel

---
+++ frontend/src/components/RightPanel/RightPanel.js
@@ -0,0 +1,70 @@
+import React, { Component, Fragment } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { GridList, GridListTile, ListSubheader, withStyles, CircularProgress, Card, CardActionArea, CardMedia, CardContent } from '@material-ui/core';
+import CardTile from 'components/CardTile/CardTile';
+
+const styles = () => ({
+    gridList: {
+        width: 300,
+    },
+    titleTile: {
+        height: '50px !important',
+        'text-align': 'center',
+        margin: '0 auto',
+    }
+});
+
+class RightPanel extends Component {
+    render() {
+        const { gridList, titleTile } = this.props.classes;
+
+        return (
+            <Fragment>
+                <GridList className={gridList}>
+                    <GridListTile className={titleTile} cols={2}>
+                        <ListSubheader component='div'>Recent Players (Click for link to discord)</ListSubheader>
+                    </GridListTile>
+                    {this.playerTiles(this.props.data, this.props.classes)}
+                </GridList>
+            </Fragment>
+        );
+    }
+
+    playerTiles(data, classes) {
+        if (data.loading || data.recentPlayers === undefined) {
+            return (
+                <GridListTile className={classes.titleTile} cols={2}>
+                    <CircularProgress/>
+                </GridListTile>
+            );
+        } else {
+            return data.recentPlayers.map(recent =>
+                <CardTile
+                    key={recent.id}
+                    active={false}
+                    pressed={this.activated.bind(this, recent.id)}
+                    icon=''
+                    {...recent}
+                />
+            );
+        }
+    }
+
+    activated(id) {
+        window.location='https://discordapp.com/users/'.concat(id);
+    }
+
+   
+}
+
+export const recentQuery = gql`{
+    data {
+        recentPlayers($token: "token") {
+            id
+            name
+        }
+    }
+}`;
+
+export default graphql(recentQuery)(withStyles(styles)(RightPanel));
